ff3a871e4d163c47944f141a9f43db6038ec1ca1
Your Name <you@example.com>
2022-05-17 17:21:50 -0500

    veroooo

--- controlador.js
+++ controlador.js
@@ -1,108 +1,470 @@
 //OBJETOS
 let producto1={
-    nombre:"producto1",
-    precio:500000,
+    nombre:"Acordeon",
+    precio:89000,
     estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/acordeon.jpg?alt=media&token=c4a54e8f-fc79-42b7-ad6f-cc2acb643f9c"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/acordeon.jpg?alt=media&token=c4a54e8f-fc79-42b7-ad6f-cc2acb643f9c",
+    popularidad:"⭐⭐⭐",
+    pais:"Colombia"
 }
 
 let producto2={
-    nombre:"producto2",
-    precio:500000,
+    nombre:"Amplificador Marshall",
+    precio:214900,
     estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/amplificador%20de%20sonido.jfif?alt=media&token=b7dce230-71fb-4df9-977d-1e8fcb70dac5"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/amplificador%20de%20sonido.jfif?alt=media&token=b7dce230-71fb-4df9-977d-1e8fcb70dac5",
+    popularidad:"⭐⭐⭐",
+    pais:"China"
 }
 
 let producto3={
-    nombre:"producto3",
-    precio:500000,
-    estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/atril%20partitura.jpg?alt=media&token=da125537-3937-4c7a-96a4-d5ff039f4776"
+    nombre:"Atril De Bandeja Para Partitura",
+    precio:52000,
+    estado:"Viejo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/atril%20partitura.jpg?alt=media&token=da125537-3937-4c7a-96a4-d5ff039f4776",
+    popularidad: "⭐⭐",
+    pais:"Estados Unidos"
 }
 
 let producto4={
-    nombre:"producto4",
-    precio:500000,
-    estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/bateria%20acustica...jfif?alt=media&token=204c33ae-213e-4ccc-9ed9-0e2366bece99"
+    nombre:"Bateria Acustica Tama Stagestar Sg52kh5",
+    precio:1900000,
+    estado:"Viejo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/bateria%20acustica...jfif?alt=media&token=204c33ae-213e-4ccc-9ed9-0e2366bece99",
+    popularidad: "⭐⭐⭐⭐",
+    pais:"Arabia Saudita"
 }
 
 let producto5={
-    nombre:"producto5",
-    precio:500000,
+    nombre:"Bateria Electronica / Electrica Alesis Nitro Mesh",
+    precio:2618000,
     estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/bateria%20electrica.jfif?alt=media&token=25281c6d-e50a-4d19-9785-fd451b694068"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/bateria%20electrica.jfif?alt=media&token=25281c6d-e50a-4d19-9785-fd451b694068",
+    popularidad: "⭐⭐⭐",
+    pais:"Estados Unidos"
 }
 
 let producto6={
-    nombre:"producto6",
-    precio:500000,
+    nombre:"Cajon Flamenco Peruano6",
+    precio:273000,
     estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/caja%20peruana.jpg?alt=media&token=66bcc255-8ea0-4ace-812a-9c68bfb41f86"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/caja%20peruana.jpg?alt=media&token=66bcc255-8ea0-4ace-812a-9c68bfb41f86",
+    popularidad:"⭐⭐",
+    pais:"Peru"
 }
 
 let producto7={
-    nombre:"producto7",
-    precio:500000,
+    nombre:"Consola Mezclador",
+    precio:678000,
     estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/consolade%20sonido.jfif?alt=media&token=63bc8a75-be77-448f-8740-6d7bf899580a"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/consolade%20sonido.jfif?alt=media&token=63bc8a75-be77-448f-8740-6d7bf899580a",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Estados Unidos"
 }
 
 let producto8={
-    nombre:"producto8",
-    precio:500000,
+    nombre:"Guitarra acústica",
+    precio:420000,
     estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/guitarra%20acustica.jpg?alt=media&token=5a947332-ed70-4c60-8670-d9ca18e8d2bb"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/guitarra%20acustica.jpg?alt=media&token=5a947332-ed70-4c60-8670-d9ca18e8d2bb",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:"China"
 }
 
 let producto9={
-    nombre:"producto9",
-    precio:500000,
-    estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/guitarra%20electrica.jpg?alt=media&token=975c7cb4-0308-404c-ab07-d7b655ba30a1"
+    nombre:"Guitarra eléctrica Ibanez RG GIO GRX40",
+    precio:523300,
+    estado:"Viejo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/guitarra%20electrica.jpg?alt=media&token=975c7cb4-0308-404c-ab07-d7b655ba30a1",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:"Japon"
 }
+
 
 let producto10={
-    nombre:"producto10",
-    precio:500000,
+    nombre:"Maracas Salseras",
+    precio:68000,
     estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/maracas.jpg?alt=media&token=b07b6fc6-b4f9-4b4c-989f-dbf13e7bae39"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/maracas.jpg?alt=media&token=b07b6fc6-b4f9-4b4c-989f-dbf13e7bae39",
+    popularidad:"⭐⭐",
+    pais:"Colombia"
 }
+
 
 let producto11={
-    nombre:"producto11",
-    precio:500000,
-    estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/microfono.jpg?alt=media&token=b540d276-e039-424a-9bf3-3193ce71e1ce"
+    nombre:"Micrófono Shure PG Alta PGA48-XLR",
+    precio:124900,
+    estado:"Viejo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/microfono.jpg?alt=media&token=b540d276-e039-424a-9bf3-3193ce71e1ce",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Rusia"
 }
+
 
 let producto12={
-    nombre:"producto12",
-    precio:500000,
+    nombre:"Piano Acustico Steinway",
+    precio:12936000,
     estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/piano%20acustico.jfif?alt=media&token=26e8dca7-3769-4cb9-a93a-969403b889f4"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/piano%20acustico.jfif?alt=media&token=26e8dca7-3769-4cb9-a93a-969403b889f4",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Inglaterra"
 }
+
 
 let producto13={
-    nombre:"producto13",
-    precio:500000,
+    nombre:"Teclado Organeta Yamaha",
+    precio:816000,
     estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/piano.png?alt=media&token=4b369b19-7197-4a53-bb99-0cc116d6d8b9"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/piano.png?alt=media&token=4b369b19-7197-4a53-bb99-0cc116d6d8b9",
+    popularidad:"⭐⭐⭐",
+    pais:"Estados Unidos "
 }
+
 
 let producto14={
-    nombre:"producto14",
-    precio:500000,
-    estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/puntillas%20de%20guitarra.jfif?alt=media&token=b6714feb-ccd8-472d-bd60-336f54046f6b"
+    nombre:"Picks - Pajuela - Pua Alice Ap-100 Guitarra 3 Unidades",
+    precio:2000,
+    estado:"Viejo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/puntillas%20de%20guitarra.jfif?alt=media&token=b6714feb-ccd8-472d-bd60-336f54046f6b",
+    popularidad:"⭐⭐",
+    pais:"Mexico"
 }
 
+
 let producto15={
-    nombre:"producto15",
-    precio:500000,
+    nombre:"Redoblante Metálico De 14 Pulgadas 8 Herrajes",
+    precio:379900,
     estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/redoblante%20sinfonico.jpg?alt=media&token=a5c577e3-46ac-4247-9725-47e862a35dbc"
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinica-del-play-a68aa.appspot.com/o/redoblante%20sinfonico.jpg?alt=media&token=a5c577e3-46ac-4247-9725-47e862a35dbc",
+    popularidad:"⭐⭐⭐",
+    pais:"Inglaterra"
 }
+
+let producto16={
+    nombre:"Baquetas Para Bateria",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto17={
+    nombre:"Violin Acustico",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto18={
+    nombre:"Violin Electrico",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto19={
+    nombre:"Sintetizador",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto20={
+    nombre:"Base Para Teclado",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto21={
+    nombre:"Libros De Partituras",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto22={
+    nombre:"Libro Pentagramado",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto23={
+    nombre:"Campana De Mano",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto24={
+    nombre:"Flauta Dulce",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto25={
+    nombre:"Melodica Teclado",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto26={
+    nombre:"Bongo",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto27={
+    nombre:"Bongo De Mano",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto28={
+    nombre:"Tambor",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto29={
+    nombre:"Pandereta",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto30={
+    nombre:"Xilofono",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto31={
+    nombre:"Marimba",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto32={
+    nombre:"Arpa",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto33={
+    nombre:"Arco De Violin",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto34={
+    nombre:"Cuerdas De Guitarra",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto35={
+    nombre:"Trompeta",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto36={
+    nombre:"Trombon",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto37={
+    nombre:"Corno",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto38={
+    nombre:"Tuba",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto39={
+    nombre:"saxsofon",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto40={
+    nombre:"ukelele",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto41={
+    nombre:"",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto42={
+    nombre:"",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto43={
+    nombre:"",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto44={
+    nombre:"",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto45={
+    nombre:"",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto46={
+    nombre:"",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto47={
+    nombre:"",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto48={
+    nombre:"",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto49={
+    nombre:"",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+let producto50={
+    nombre:"",
+    precio:00000,
+    estado:"Nuevo",
+    foto:"",
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:""
+}
+
+
+
+
+
+
+
+
+
+
+
 
 //ARREGLO
 let productos=Array(
@@ -120,7 +482,42 @@ let productos=Array(
     producto12,
     producto13,
     producto14,
-    producto15
+    producto15,
+    producto16,
+    producto17,
+    producto18,
+    producto19,
+    producto20,
+    producto21,
+    producto22,
+    producto23,
+    producto25,
+    producto26,
+    producto27,
+    producto28,
+    producto29,
+    producto30,
+    producto31,
+    producto32,
+    producto33,
+    producto34,
+    producto35,
+    producto36,
+    producto37,
+    producto38,
+    producto39,
+    producto40,
+    producto41,
+    producto42,
+    producto43,
+    producto44,
+    producto45,
+    producto46,
+    producto47,
+    producto48,
+    producto49,
+    producto50,
+
 )
 
 //RECORRO EL ARREGLO
@@ -130,11 +527,41 @@ productos.forEach(function(producto){
     let foto = document.createElement("img")
     foto.classList.add("w-100" , "img-fluid")
     foto.src = producto.foto
+
     let columna = document.createElement("div")
     columna.classList.add("col")
+
     let tarjeta = document.createElement("div")
-    tarjeta.classList.add("card")
+    tarjeta.classList.add("card","h-100","shadow")
+
+    let nombre = document.createElement("h5")
+    nombre.classList.add("text-center")
+    nombre.textContent = producto.nombre
+
+    let precio = document.createElement("h5")
+    precio.classList.add("text-center")
+    precio.textContent = ("$")+producto.precio
+
+    let estado = document.createElement("h5")
+    estado.classList.add("text-center")
+    estado.textContent = producto.estado
+
+    let popularidad = document.createElement("h5")
+    popularidad.classList.add("text-center")
+    popularidad.textContent = producto.popularidad
+
+    let pais = document.createElement("h5")
+    pais.classList.add("text-center")
+    pais.textContent = producto.pais
+
     tarjeta.appendChild(foto)
+    tarjeta.appendChild(nombre)
+    tarjeta.appendChild(precio)
+    tarjeta.appendChild(estado)
+    tarjeta.appendChild(popularidad)
+    tarjeta.appendChild(pais)
+
+
     columna.appendChild(tarjeta)
     container.appendChild(columna)
 
