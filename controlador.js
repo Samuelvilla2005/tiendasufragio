57c7e2314ba1c7f1f71ae8107c27d52cbcd4cf93
Your Name <you@example.com>
2022-05-18 17:18:18 -0500

    veronicamiamor

--- controlador.js
+++ controlador.js
@@ -142,317 +142,317 @@ let producto15={
 
 let producto16={
     nombre:"Baquetas Para Bateria",
-    precio:00000,
+    precio:64900,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/baquetas%20bateria.jpg?alt=media&token=2f73d8ad-4892-4e2d-bb3f-7a24782e1953",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"China"
 }
 
 let producto17={
     nombre:"Violin Acustico",
-    precio:00000,
+    precio:230000,
     estado:"Nuevo",
-    foto:"",
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/Violin_VL100.png?alt=media&token=c3eb82de-658b-4a82-a181-e23399b70ccf",
     popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    pais:"Arabia Saudita"
 }
 
 let producto18={
     nombre:"Violin Electrico",
-    precio:00000,
+    precio:648000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/violin%20electrico.jpg?alt=media&token=d12bf874-e3b2-42a2-8b6f-a7769e46c963",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Canada"
 }
 
 let producto19={
     nombre:"Sintetizador",
-    precio:00000,
+    precio:6490000,
     estado:"Nuevo",
-    foto:"",
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/sintetizador.jfif?alt=media&token=427e4589-9f53-4bd3-8687-377ce6e969f6",
     popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    pais:"Estados Unidos"
 }
 
 let producto20={
     nombre:"Base Para Teclado",
-    precio:00000,
+    precio:57000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/Base%20para%20piano.jfif?alt=media&token=bd048b61-e530-43c5-8386-49c445443d6a",
+    popularidad:"⭐⭐⭐",
+    pais:"Colombia"
 }
 
 let producto21={
     nombre:"Libros De Partituras",
-    precio:00000,
+    precio:35000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/libro%20de%20partiturAS.jpg?alt=media&token=9835887b-a311-43cb-8a4a-7d9b519ed540",
+    popularidad:"⭐⭐⭐",
+    pais:"Italia"
 }
 
 let producto22={
     nombre:"Libro Pentagramado",
-    precio:00000,
+    precio:35000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/libro%20pentagramado.png?alt=media&token=f08b5cce-f793-4d84-bceb-14c12e550da3",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Inglaterra"
 }
 
 let producto23={
     nombre:"Campana De Mano",
-    precio:00000,
+    precio:59000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/campana%20de%20mano.jpg?alt=media&token=83fcd1df-d9ff-4389-8ba0-b2c3c8e272b4",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Francia"
 }
 
 let producto24={
     nombre:"Flauta Dulce",
-    precio:00000,
+    precio:27000,
     estado:"Nuevo",
-    foto:"",
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/flauta%20dulce.jpg?alt=media&token=a110ac66-e9cd-452a-9a25-b9eb4a5cee5e",
     popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    pais:"Francia"
 }
 
 let producto25={
     nombre:"Melodica Teclado",
-    precio:00000,
+    precio:99000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/melodica%20teclado.jpg?alt=media&token=bf500f49-7d8f-490a-bcde-dbef540c1477",
+    popularidad:"⭐⭐⭐",
+    pais:"Estados Unidos"
 }
 
 let producto26={
     nombre:"Bongo",
-    precio:00000,
+    precio:230000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/bongo.jpg?alt=media&token=d9e161ea-8953-4544-be1c-16b98f581080",
+    popularidad:"⭐⭐⭐",
+    pais:"Puerto Rico"
 }
 
 let producto27={
     nombre:"Bongo De Mano",
-    precio:00000,
+    precio:250000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/bongo%20de%20mano.jpg?alt=media&token=4609da46-7f3c-4f67-a7ee-12d7118a12c0",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Canada"
 }
 
 let producto28={
     nombre:"Tambor",
-    precio:00000,
+    precio:280000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/tambor.jpg?alt=media&token=61358009-e8c8-4836-b94e-3d9395f983b9",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Italia"
 }
 
 let producto29={
     nombre:"Pandereta",
-    precio:00000,
+    precio:35000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/pandereta.jpg?alt=media&token=8ed156fe-d422-4f92-b31d-4544a1e61b03",
+    popularidad:"⭐⭐⭐",
+    pais:"Italia"
 }
 
 let producto30={
     nombre:"Xilofono",
-    precio:00000,
+    precio:72000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/xilofono.jpg?alt=media&token=47e3ae1a-869f-4039-a0a1-45c66effb425",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Francia"
 }
 
 let producto31={
     nombre:"Marimba",
-    precio:00000,
+    precio:250000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/marimba.jpg?alt=media&token=3eec4978-a374-47fc-90fc-08d0c5875dbf",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Estados Unidos"
 }
 
 let producto32={
     nombre:"Arpa",
-    precio:00000,
+    precio:470000,
     estado:"Nuevo",
-    foto:"",
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/arpa.jpg?alt=media&token=5d06f8f2-f99f-4d34-909f-038c49b8ea58",
     popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    pais:"Arabia Saudita"
 }
 
 let producto33={
     nombre:"Arco De Violin",
-    precio:00000,
+    precio:49000,
     estado:"Nuevo",
-    foto:"",
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/arco%20de%20violin.png?alt=media&token=3a61dcd0-cd28-4bb6-b232-2ad5db79a46f",
     popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    pais:"China"
 }
 
 let producto34={
     nombre:"Cuerdas De Guitarra",
-    precio:00000,
+    precio:30000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/cuerdas%20guitarra.jpg?alt=media&token=16f9dcd4-21ec-459c-b990-c30226bebabd",
+    popularidad:"⭐⭐⭐",
+    pais:"China"
 }
 
 let producto35={
     nombre:"Trompeta",
-    precio:00000,
+    precio:705000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/trompeta.jpg?alt=media&token=e40873a0-af6a-4e04-94bf-d0ad2f004576",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Japon"
 }
 
 let producto36={
     nombre:"Trombon",
-    precio:00000,
+    precio:308000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/trombon.jpg?alt=media&token=4f3b69b9-0a69-46e5-ae5d-68de451fb16b",
+    popularidad:"⭐⭐⭐",
+    pais:"Estados Unidos"
 }
 
 let producto37={
     nombre:"Corno",
-    precio:00000,
+    precio:350000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/corno.jpg?alt=media&token=5c344fd5-005f-448c-894d-e4e1f11b270d",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Japon"
 }
 
 let producto38={
     nombre:"Tuba",
-    precio:00000,
+    precio:230000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/tuba.jfif?alt=media&token=01ef2f14-af25-46a8-97d0-69691b0a25c0",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Colombia"
 }
 
 let producto39={
-    nombre:"saxsofon",
-    precio:00000,
+    nombre:"Saxofon",
+    precio:1300000,
     estado:"Nuevo",
-    foto:"",
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/saxofon.jpg?alt=media&token=f38d9ab6-e6d7-4f66-93ba-074ab9de71f0",
     popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    pais:"Francia"
 }
 
 let producto40={
-    nombre:"ukelele",
-    precio:00000,
+    nombre:"Ukelele",
+    precio:150000,
     estado:"Nuevo",
-    foto:"",
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/ukelele.jpg?alt=media&token=7a7cf956-4945-47b7-a35c-1144487c334d",
     popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    pais:"Estados Unidos"
 }
 
 let producto41={
-    nombre:"",
-    precio:00000,
+    nombre:"Estuche Para Guitarra",
+    precio:40000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/estuche%20para%20guitarra.jpg?alt=media&token=84824199-6ebb-4d85-aa0b-1a31a1bf6bf2",
+    popularidad:"⭐⭐⭐",
+    pais:"Puerto Rico"
 }
 
 let producto42={
-    nombre:"",
-    precio:00000,
+    nombre:"Cable Alimentacion",
+    precio:15000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/cacle%20alimentacion.jpg?alt=media&token=60f5a164-a459-4d7f-9da8-ca38f8108e21",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Mexico"
 }
 
 let producto43={
-    nombre:"",
-    precio:00000,
+    nombre:"Cable Entrada Amplificador",
+    precio:20000,
     estado:"Nuevo",
-    foto:"",
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/cable%20alimentacion.jpg?alt=media&token=5e374aed-4d54-48f5-b164-a23948d47d4f",
     popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    pais:"Australia"
 }
 
 let producto44={
-    nombre:"",
-    precio:00000,
+    nombre:"Audifonos Diadema",
+    precio:90000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/audifonos%20diadema.jpg?alt=media&token=3c3f49ae-d156-4bf9-87f5-83c98b51db2e",
+    popularidad:"⭐⭐⭐",
+    pais:"Rusia"
 }
 
 let producto45={
-    nombre:"",
-    precio:00000,
+    nombre:"Soporte/Brazo Para Microfono",
+    precio:99000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/brazo%20para%20audifonos.jpg?alt=media&token=05b9160b-fbf8-4a79-8a08-c71cf89ad47f",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Japon"
 }
 
 let producto46={
-    nombre:"",
-    precio:00000,
+    nombre:"Filtro Pop",
+    precio:58000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/filtro%20pop.jfif?alt=media&token=24766724-9a0a-4092-97ff-afa22a0aea3d",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Japon"
 }
 
 let producto47={
-    nombre:"",
-    precio:00000,
+    nombre:"Escudo Aislamiento",
+    precio:280000,
     estado:"Nuevo",
-    foto:"",
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/esscucdo%20aislamiento.jpg?alt=media&token=9df6e703-5301-46d5-9365-f42d13508639",
     popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    pais:"Italia"
 }
 
 let producto48={
-    nombre:"",
-    precio:00000,
+    nombre:"Amplificador Audifonos",
+    precio:189000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/amplificador%20audifonos.jfif?alt=media&token=b32ea1c5-5d43-4e23-83e5-1a133699ea49",
+    popularidad:"⭐⭐⭐⭐",
+    pais:"Colombia"
 }
 
 let producto49={
-    nombre:"",
-    precio:00000,
+    nombre:"Metronomo",
+    precio:49000,
     estado:"Nuevo",
-    foto:"",
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/metronomo.jpg?alt=media&token=3ad469fa-9a5f-454b-9fa3-2da7fe13dd7f",
     popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    pais:"China"
 }
 
 let producto50={
-    nombre:"",
-    precio:00000,
+    nombre:"Afinador Cromatico",
+    precio:20000,
     estado:"Nuevo",
-    foto:"",
-    popularidad:"⭐⭐⭐⭐⭐",
-    pais:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/musica-63271.appspot.com/o/afinador%20cromatico.jfif?alt=media&token=fd66ef45-f963-41de-9bff-55c5ec0fc900",
+    popularidad:"⭐⭐⭐",
+    pais:"Canada"
 }
 
 
